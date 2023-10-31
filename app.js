8846e77aff30c31d9bf42fc43a3da4fb20f73513
Aryaman <pantherdox@gmail.com>
2023-11-01 00:07:53 +0530

    firebase added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
   // Import the functions you need from the SDKs you need
+  import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-database.js";
+  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
+
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-  import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
-  import { getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
     // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
   
@@ -19,24 +20,28 @@
     };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const db = getDatabase();
-  const auth = getAuth(app);
-
-  //variables
-  let Email= document.getElementById('email');
-  let Password = document.getElementById('password');
-
-  //register
-  let RegisterUser = evt =>{
-    evt.preventDefault();
-
-    createUserWithEmailAndPassword(auth, Email.value, Password.value)
-    .then((credentials)=>{
-
-    })
-    .catch((error)=>{
-      console(error)
-    })
-  }
-  
+  // const app = initializeApp(firebaseConfig);
+  // const database = getDatabase(app);
+  // const auth = getAuth();
+  const signup = document.querySelector('#signup');
+  signup.addEventListener('click', (e) => {
+    console.log("chal ja bc");
+  //   var email = document.getElementById('email').value;
+  //   var password = document.getElementById('password').value;
+    
+  //   createUserWithEmailAndPassword(auth, email, password)
+  // .then((userCredential) => {
+  //   // Signed in 
+  //   const user = userCredential.user;
+  //   set(ref(database, 'users/' + user.uid), {
+  //     email: email,
+  //   });
+  //   alert("user created!");
+  //   // ...
+  // })
+  // .catch((error) => {
+  //   const errorCode = error.code;
+  //   const errorMessage = error.message;
+  //   alert(errorMessage);
+  //   // ..
+  });
